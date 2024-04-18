05a897ac9bcaf25d1987355e3d227281e49b3c35
Éverton Carvalho <evertonsnake@gmail.com>
2024-04-18 15:02:33 -0300

    test: ensure SignUpController calls EmailsValitador with correct emal

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -8,8 +8,8 @@ interface SutTypes{
 	sut: SignUpController
 	emailValidatorStub: EmailValidator
 }
-const makeSut = (): SutTypes => {
 
+const makeSut = (): SutTypes => {
 	class EmailValidatorStub implements EmailValidator {
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		isValid(email: string): boolean{
@@ -95,5 +95,20 @@ describe("SignUp Controller", () => {
 		const httpResponse = sut.handle(httpRequest);
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new InvalidParamsError("email"));
+	});
+
+	test("Should call EmailValidator with correct email", () => {
+		const {sut,emailValidatorStub} = makeSut() 
+		const isValidSpy =	jest.spyOn(emailValidatorStub, 'isValid')
+		const httpRequest = {
+			body: {
+				name: "any_name",
+				email: "any_email@email.com",
+				password: "any_password", 
+				passwordConfirmation: "any_password", 
+			},
+		};
+		sut.handle(httpRequest);
+		expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com');
 	});
 });
