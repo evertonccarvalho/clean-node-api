81ad168f2eaaf192bbd8a3a5f6e7025ff19629fc
Éverton Carvalho <evertonsnake@gmail.com>
2024-04-22 13:22:19 -0300

    test: ensure EmailValidatoAdapter returns true if validator returns true

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,7 +1,7 @@
-import {EmailValidator} from "../presentation/protocols/email-validator"
-export class EmailValidatorAdapter implements EmailValidator{
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  isValid(email: string): boolean {
-    return false
-  }
+import validator from 'validator';
+import { EmailValidator } from '../presentation/protocols/email-validator';
+export class EmailValidatorAdapter implements EmailValidator {
+	isValid(email: string): boolean {
+		return validator.isEmail(email);
+	}
 }
